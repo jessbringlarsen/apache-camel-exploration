9128b964797bab52a4571fc48a8ff1d3b64f1475
Jess Bring-Larsen <jje@vp.dk>
2020-08-03 11:22:46 +0200

    Cleanup

--- src/test/java/dk/bringlarsen/apachecamel/ftp/CamelFTPComponentTest.java
+++ src/test/java/dk/bringlarsen/apachecamel/ftp/CamelFTPComponentTest.java
@@ -19,23 +19,42 @@ public class CamelFTPComponentTest extends BaseCamelFTPComponentTest {
 
     @Test
     public void testFileContentIsRead() throws Exception {
-        final AtomicReference<String> response = new AtomicReference<>();
-        final AtomicReference<String> filename = new AtomicReference<>();
+
+        final AtomicReference<Callback> response = new AtomicReference<>();
         addRouteAndStartContext(new RouteBuilder() {
             @Override
             public void configure() {
-                from(String.format("sftp://user@localhost:%s/out?password=secret&localWorkDirectory=target/workdir&move=.done", sftpServer.getPort()))
+                from(String.format("sftp://user@localhost:%s/out?password=secret&localWorkDirectory=target/workdir&move=.done&readLock=changed&fastExistsCheck=true", sftpServer.getPort()))
                         .convertBodyTo(String.class)
-                        .log(LoggingLevel.DEBUG, "Got: ${file:name} with content: ${body}")
-                        .process(e -> filename.set(e.getIn().getHeader("CamelFilename", String.class)))
-                        .process(e -> response.set(e.getIn().getBody(String.class)));
+                        .log(LoggingLevel.INFO, "Got: ${file:name} with content: ${body}")
+                        .process(e -> response.set(new Callback(
+                                e.getIn().getHeader("CamelFilename", String.class),
+                                e.getIn().getBody(String.class))));
             }
         });
 
         uploadFileToFTP("/out/somefile.csv", "1;2;3");
         waitForDoneFile("/out/.done/somefile.csv");
 
-        assertThat(filename.get(), is("somefile.csv"));
-        assertThat(response.get(), is("1;2;3"));
+        assertThat(response.get().getFileName(), is("somefile.csv"));
+        assertThat(response.get().getContent(), is("1;2;3"));
+    }
+
+    private class Callback {
+        private String fileName;
+        private String content;
+
+        public Callback(String fileName, String content) {
+            this.fileName = fileName;
+            this.content = content;
+        }
+
+        public String getFileName() {
+            return fileName;
+        }
+
+        public String getContent() {
+            return content;
+        }
     }
 }
